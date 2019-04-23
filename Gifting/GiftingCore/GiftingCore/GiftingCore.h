264399ac1ccec0bc8be9f8581e4706a59b640efc
Frulwinn <frulwinn@gmail.com>
2019-04-23 15:19:11 -0500

    added core cocoa framework to project

---
+++ Gifting/GiftingCore/GiftingCore/GiftingCore.h
@@ -0,0 +1,19 @@
+//
+//  GiftingCore.h
+//  GiftingCore
+//
+//  Created by Lambda_School_Loaner_34 on 4/23/19.
+//  Copyright © 2019 Frulwinn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GiftingCore.
+FOUNDATION_EXPORT double GiftingCoreVersionNumber;
+
+//! Project version string for GiftingCore.
+FOUNDATION_EXPORT const unsigned char GiftingCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GiftingCore/PublicHeader.h>
+
+
