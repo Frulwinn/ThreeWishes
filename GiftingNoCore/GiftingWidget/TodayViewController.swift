60ef020c0c6d02d7414510275cd4bed9eddd4091
Frulwinn <frulwinn@gmail.com>
2019-04-24 14:04:11 -0500

    added fetcher to widget perform update in today view controller

--- GiftingNoCore/GiftingWidget/TodayViewController.swift
+++ GiftingNoCore/GiftingWidget/TodayViewController.swift
@@ -13,18 +13,53 @@ import CoreData
 //Don't forget to say no to Rquire Only App - Extension Safe API in widget target build settings
 class TodayViewController: UIViewController, NCWidgetProviding {
     
+    //MARK: - Properties
+    let fetcher = PersonController()
+    var person: Person?
+    /*
+     var symbol: String {
+        return "GBP"
+    */
+    
     //MARK: - Outlets
+    @IBOutlet weak var birthdayLabel: UILabel!
     @IBOutlet weak var nameLabel: UILabel!
     @IBOutlet weak var giftLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        //extends widget
+        extensionContext?.widgetLargestAvailableDisplayMode = .expanded
 
     }
         
     func widgetPerformUpdate(completionHandler: (@escaping (NCUpdateResult) -> Void)) {
         // Perform any setup necessary in order to update the view.
+        // need birthday, name, firstchoice
         
+        fetcher.fetchPersonFromServer { (error) in
+            if let error = error {
+                NSLog("Error fetching current person: \(error)")
+                completionHandler(.failed)
+                return
+            }
+            
+            guard let person = self.person,
+                let birthday = person.birthday else {
+                completionHandler(.failed)
+                return
+            }
+            
+            let dateFormatter = DateFormatter()
+            dateFormatter.dateFormat = "MMMM d"
+            
+            DispatchQueue.main.async {
+                self.birthdayLabel.text = dateFormatter.string(from: birthday)
+                self.nameLabel.text = person.name
+                self.giftLabel.text = person.firstChoice
+            }
+        }
         
         completionHandler(NCUpdateResult.newData)
     }
